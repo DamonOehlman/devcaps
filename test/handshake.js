abf400e56d904d8ca7480df7623d61ffa3eb3e70
Damon Oehlman <damon.oehlman@sidelab.com>
2012-05-29 18:46:25 +1000

    Making some progress on the revised implementation

--- test/handshake.js
+++ test/handshake.js
@@ -1,15 +1,31 @@
 var server = require('./helpers/server'),
-    request = require('request');
-
+    request = require('request'),
+    expect = require('expect.js'),
+    testUrl = 'http://localhost:3000/test';
+    
 describe('devcaps capability tests', function() {
     before(function(done) {
         server({}, done);
     });
 
     it('should display a caps detection page when cookies have not been detected', function(done) {
-        request('http://localhost:3000/test', function(err, res) {
-            console.log(res);
+        request(testUrl, function(err, res) {
+            expect(err).to.not.be.ok();
+            expect(res.body).to.contain('id="detector"');
             done();
+        });
+    });
+
+    it('should set a cookie when it receives devcaps data in a post', function(done) {
+        var requestData = {
+                url: testUrl,
+                data: {
+                    devcaps: 'FBX'
+                }
+            };
+        
+        request.post(requestData, function(err, res) {
+            // console.log(res);
         });
     });
 });
