f18f244977285c8d5d7ee2be682fc7d5e6562d1b
Damon Oehlman <damon.oehlman@sidelab.com>
2012-05-30 19:17:55 +1000

    Updated test runner

--- test/caps.js
+++
@@ -1,9 +0,0 @@
-var server = require('./helpers/server'),
-    request = require('request');
-
-describe('devcaps capability tests', function() {
-    before(function(done) {
-        server({}, done);
-    });
-    
-});
